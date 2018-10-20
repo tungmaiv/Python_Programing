6bc86eb050f55d37946d135d2471435a61a4616d
Tung Mai Vu <tungvm@HOSO932.local>
2018-10-20 12:03:25 +0700

    commit

---
+++ dvm-tutorialspoint/1.3-variable-types.py
@@ -0,0 +1,16 @@
+import datetime
+
+name = ("Jeff")
+number = ("100")
+favoriteFood = ("pasta")
+a,b,c = 1,2,3
+
+print (name)
+print (number)
+print (favoriteFood)
+print (a)
+print (b)
+print (c)
+dt = datetime.date(2018,10,20)
+wk = dt.isocalendar()[1]
+print(wk)
