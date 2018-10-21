a5d3a670128d731ed03d95f6ad3a4ea3eb336e17
Duc Vu Minh <ducvm@Ducs-MacBook-Pro.local>
2018-10-21 21:54:37 +0700

    commit

---
+++ dvm-tutorialspoint/1.4-basic-operators.py
@@ -0,0 +1,33 @@
+import sys
+print (sys.executable)
+print (sys.version)
+a,b,c,d,e = 14,25,4,0.5,14
+x =(1,2,3,4,5,6)
+y =(6,7,8,9,10,11)
+z =(1,2,3,4,5,6)
+q =(1,2,3)
+print (a+b)
+print (b-c)
+print (a*c)
+print (10/d)
+print ( b%a)
+print (a**c)
+print (b//c)
+
+print (a==b)
+print (a!=c)
+print (a > e)
+print (b<d)
+print (a>=e)
+print (c<=e)
+
+c =+ a
+print ("Value of c is ", c)
+c -= b
+print ("Value of c is ", c)
+
+print (x is y)
+print (y is not z)
+
+print (x in y)
+print (1 in z)
