fae5ac837b51aa7f6c9e33bb260a7c2dcb0744d3
Tung Vu Mai <tungvm@Admins-MacBook-Pro.local>
2018-10-26 00:21:31 +0700

    commit

---
+++ Attitude.py
@@ -0,0 +1,17 @@
+
+att = "Attitude"
+itl = "Intelligent"
+klg = "Knowledge"
+
+total_att = 0; total_itl = 0; total_klg = 0
+
+for letter in att.upper():
+    total_att += ord(letter)
+for letter in itl.upper():
+    total_itl += ord(letter)
+for letter in klg.upper():
+    total_klg += ord(letter)
+
+print("Total of Attitude is: {}".format(total_att/len(att)))
+print("Total of Intelligent is: {}".format(total_itl/len(itl)))
+print("Total of Knowledge is: {}".format(total_klg/len(klg)))
