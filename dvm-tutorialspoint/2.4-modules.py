bf16fac0e5de6afd13ee7366bea50569e981730d
Duc Vu Minh <ducvm@Ducs-MacBook-Pro.local>
2018-10-31 21:51:51 +0700

    commit

---
+++ dvm-tutorialspoint/2.4-modules.py
@@ -0,0 +1,10 @@
+import sys
+import math
+print (sys.version)
+print (sys.executable)
+print ("\n")
+def print_func(par):
+    print ("Hello", par)
+    return
+import support
+support.print_func ("Jeff")
